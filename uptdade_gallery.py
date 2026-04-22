8c5017993f00447b273876846e912d165145f7bf
wen1538 <wenceslas2002@gmail.com>
2026-04-22 20:31:55 +0200

    Add files via upload

--- uptdade_gallery.py
+++ uptdade_gallery.py
@@ -13,15 +13,11 @@ def save_db(data):
         json.dump(data, f, indent=4, ensure_ascii=False)
 
 def add_photo(id, url, titre, lieu, rubrique="Pomme", iso="ISO 100", focale="35mm", ouverture="f/2.8"):
-    """
-    Rubriques valides : 'Journal', 'View On', 'Eponyme', 'Pomme'
-    """
     db = load_db()
     db = [i for i in db if i['id'] != id]
     db.append({
         "id": id, "url": url, "titre": titre, "lieu": lieu, 
-        "rubrique": rubrique, "iso": iso, "focale": focale, 
-        "ouverture": ouverture
+        "rubrique": rubrique, "iso": iso, "focale": focale, "ouverture": ouverture
     })
     save_db(db)
-    print(f"✅ Élément '{titre}' ajouté avec succès.")
+    print(f"✅ Ajouté à {rubrique}.")
